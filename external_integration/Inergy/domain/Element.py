17e8ff63b9d14686f8ddd43f5521e35e96e716bc
jose <manoloainsa@gmail.com>
2023-06-29 14:03:32 +0200

    zlin region filter

--- external_integration/Inergy/domain/Element.py
+++ external_integration/Inergy/domain/Element.py
@@ -20,22 +20,37 @@ class Element(object):
     location: Optional[dict] = None
 
     @classmethod
-    def create(cls, id_project, i):
+    def create(cls, id_project, i, user):
         print(i)
         building = i['b']
         location = i['lo']
         city = i['c']
-
-        if all(item in list(building.keys()) for item in
-               ['bigg__buildingIDFromOrganization', 'bigg__buildingName']) \
-                and all(item in list(location.keys()) for item in
-               ['bigg__addressStreetName']):
-            building_name = f"{building.get('bigg__buildingName')}-{location.get('bigg__addressStreetName')}"
-            building_name = f"{building_name}-{location.get('bigg__addressStreetNumber')}" if location.get('bigg__addressStreetNumber') else building_name
-            return Element(id_project=id_project,
-                           instance=1,
-                           code=str(building.get('bigg__buildingIDFromOrganization')),
-                           use='Equipment', typology=9, name=building_name,
-                           begin_date=str(date(2019, 1, 1)),
-                           end_date=str(date.today() + relativedelta(years=10)),
-                           location=Location.create(location, city).__dict__)
+        building_name = ""
+        if user == "czech":
+            if id_project == '1142':
+                if all(item in list(building.keys()) for item in
+                       ['bigg__buildingIDFromOrganization', 'bigg__buildingName']) \
+                        and all(item in list(city.keys()) for item in
+                                ['geo__name']):
+                    building_name = f"{city.get('geo__name')}-{building.get('bigg__buildingName')}"
+                    print(building_name)
+            else:
+                if all(item in list(building.keys()) for item in
+                       ['bigg__buildingIDFromOrganization', 'bigg__buildingName']):
+                    building_name = building.get('bigg__buildingName')
+                    print(building_name)
+        else:
+            if all(item in list(building.keys()) for item in
+                   ['bigg__buildingIDFromOrganization', 'bigg__buildingName']) \
+                    and all(item in list(location.keys()) for item in
+                   ['bigg__addressStreetName']):
+                building_name = f"{building.get('bigg__buildingName')}-{location.get('bigg__addressStreetName')}"
+                building_name = f"{building_name}-{location.get('bigg__addressStreetNumber')}" if location.get('bigg__addressStreetNumber') else building_name
+                print(building_name)
+        return Element(id_project=id_project,
+                       instance=1,
+                       code=str(building.get('bigg__buildingIDFromOrganization')),
+                       use='Equipment', typology=9, name=building_name,
+                       begin_date=str(date(2019, 1, 1)),
+                       end_date=str(date.today() + relativedelta(years=10)),
+                       location=Location.create(location, city).__dict__)
